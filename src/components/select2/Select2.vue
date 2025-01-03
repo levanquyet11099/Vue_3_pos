1b12b849a498460f776a03c1b0be3a1561c37cbd
levanquyet11099 <lyquyetvanle@gmail.com>
2025-01-03 18:13:46 +0700

    upddateupddate

--- src/components/select2/Select2.vue
+++ src/components/select2/Select2.vue
@@ -1,51 +1,83 @@
 <template>
-  <div>
-    <select ref="select" class="form-select" style="width: 100%">
-      <option v-for="option in options" :key="option.id" :value="option.id">
-        {{ option.text }}
-      </option>
-    </select>
-  </div>
+  <select ref="select" class="w-full">
+    <option v-for="option in options" :key="option.id" :value="option.id">
+      {{ option.text }}
+    </option>
+  </select>
 </template>
 
-  <script lang="ts" setup>
+<script lang="ts" setup>
+import { ref, onMounted, onBeforeUnmount, watch, toRefs } from 'vue'
 import $ from 'jquery'
 import 'select2/dist/css/select2.min.css'
 import 'select2'
 
-export default {
-  name: 'SelectTwo',
-  props: {
-    options: {
-      type: Array,
-      required: true,
-    },
-    value: {
-      type: [String, Number, Array],
-      default: null,
-    },
+// Định nghĩa props
+const props = defineProps({
+  options: {
+    type: Array as PropType<Array<{ id: string | number; text: string }>>,
+    required: true,
   },
-  mounted() {
-    // $(this.$refs.select).select2().val(this.value).trigger('change')
+  modelValue: {
+    type: [String, Number, Array],
+    default: null,
+  },
+})
+const emit = defineEmits(['update:modelValue'])
 
-    $(this.$refs.select).on('change', () => {
-      this.$emit('input', $(this.$refs.select).val())
-    })
-  },
-  watch: {
-    value(value) {
-      $(this.$refs.select).val(value).trigger('change')
-    },
-    options(options) {
-      $(this.$refs.select).select2({ data: options })
-    },
-  },
-  beforeUnmount() {
-    $(this.$refs.select).select2('destroy')
-  },
+// refs
+const select = ref<HTMLSelectElement | null>(null)
+
+// Hàm xử lý khi lựa chọn thay đổi
+const updateValue = () => {
+  if (select.value) {
+    emit('update:modelValue', $(select.value).val())
+  }
 }
+
+// Khởi tạo Select2
+onMounted(() => {
+  $(select.value).select2()
+
+  // Gán giá trị ban đầu
+  $(select.value).val(props.modelValue).trigger('change')
+
+  // Thêm sự kiện change
+  $(select.value).on('change', updateValue)
+})
+
+// Cập nhật giá trị khi props.modelValue thay đổi
+watch(
+  () => props.modelValue,
+  (newValue) => {
+    if (select.value) {
+      $(select.value).val(newValue).trigger('change')
+    }
+  }
+)
+
+// Cập nhật lại danh sách options
+watch(
+  () => props.options,
+  (newOptions) => {
+    if (select.value) {
+      $(select.value).empty()
+      newOptions.forEach((option) => {
+        $(select.value).append(new Option(option.text, option.id))
+      })
+      $(select.value).select2() // Cập nhật select2
+    }
+  }
+)
+
+// Hủy Select2 khi component bị unmount
+onBeforeUnmount(() => {
+  if (select.value) {
+    $(select.value).select2('destroy')
+  }
+})
 </script>
 
-  <style scoped>
-/* CSS của bạn */
+<style scoped>
+/* CSS nếu cần */
 </style>
